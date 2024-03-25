dc2101d7a9bfbed4dcf1f76e987acedce30af07f
Harsh Dakhare <121993868+Harshdakhare4@users.noreply.github.com>
2024-03-26 01:42:55 +0530

    Add files via upload

---
+++ sol4/main.js
@@ -0,0 +1,10 @@
+
+const { add, subtract, multiply, divide } = require('./math');
+
+const a = 44;
+const b = 11;
+
+console.log("Addition:", add(a, b));
+console.log("Subtraction:", subtract(a, b));
+console.log("Multiplication:", multiply(a, b));
+console.log("Division:", divide(a, b));
